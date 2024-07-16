ed008f46217706dcd72bf461fe23b7b2cf3ac51e
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-07-16 09:42:13 +0500

    creating a repo

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { CustomerReview ,PopularProduct,Services,SpecialOffers,SuperQuality,Subscribe,Footer,Hero } from "./sections";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+      </section>
+    <section className="padding">
+      <PopularProduct />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers /> 
+    </section>
+    <section className="bg-pale-blue  padding">
+<CustomerReview />   
+ </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe /> 
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+
+export default App;
