761c2dedc8777235c7e7e9d79684a7c6699a263d
hamidmeer4 <103202419+hamidmeer4@users.noreply.github.com>
2024-07-26 21:19:11 +0500

    done with sevice section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,12 @@
-
-
+import ServiceCard from '../components/ServiceCard'
+import {services} from '../constants'
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-contaainer flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+       <ServiceCard key={service.label} {...service}/>
+      ))}
+    </section>
   )
 }
 
